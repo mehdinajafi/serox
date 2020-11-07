c978c01b0da065f40e3a3dd736fb230377e21c38
Mehdi <mehdinajafi20.mn@gmail.com>
2020-11-08 00:56:11 +0330

    Login/Signup with Google Account

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import * as React from "react"
+import { firebase } from "../firebase/firebase"
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <button onClick={() => firebase.auth().signOut()}>sign out</button>
+    </div>
+  )
 }
 
 export default Home
--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,7 +1,18 @@
 import * as React from "react"
+import GoogleAuth from "../components/join/GoogleAuth"
 
 const Join = () => {
-  return <div>Join</div>
+  return (
+    <div className="container">
+      <div className="w-11/12 sm:w-3/6 mx-auto">
+        <h1 className="text-center font-bold text-2xl text-gray-900">
+          Hi, Join us
+        </h1>
+        <GoogleAuth />
+        <hr></hr>
+      </div>
+    </div>
+  )
 }
 
 export default Join
