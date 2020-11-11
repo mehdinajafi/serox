55b99294d74bf44a2957b8f3dd584e3f6fb61906
Mehdi <mehdinajafi20.mn@gmail.com>
2020-11-11 23:53:23 +0330

    Fix bug in Login component

--- src/components/join/Login.tsx
+++ src/components/join/Login.tsx
@@ -1,43 +1,44 @@
 import * as React from "react"
 import { withRouter, RouteComponentProps } from "react-router-dom"
 import { firebase } from "../../firebase/firebase"
+import { ReactComponent as ErrorLogo } from "../../assets/icons/error.svg"
 
 interface LoginProps extends RouteComponentProps<any> {}
 
 const Login: React.FC<LoginProps> = ({ history }) => {
-  const submitForm = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const [
-      userNameInput,
-      emailInput,
-      passwordInput,
-    ]: HTMLFormControlsCollection = (e.target as HTMLFormElement).elements
-    await firebase
-      .auth()
-      .signInWithEmailAndPassword(
-        (emailInput as HTMLInputElement).value,
-        (passwordInput as HTMLInputElement).value
-      )
-      .then((userCredential) => {
-        if (userCredential.user?.displayName) {
-          userCredential.user.displayName = (userNameInput as HTMLInputElement).value
+  const [error, setError] = React.useState<null | string>(null)
+
+  const submitForm = React.useCallback(
+    async (e: React.FormEvent) => {
+      e.preventDefault()
+      const [
+        emailInput,
+        passwordInput,
+      ]: HTMLFormControlsCollection = (e.target as HTMLFormElement).elements
+      if (
+        (emailInput as HTMLInputElement).value.trim() &&
+        (passwordInput as HTMLInputElement).value.trim()
+      ) {
+        try {
+          await firebase
+            .auth()
+            .signInWithEmailAndPassword(
+              (emailInput as HTMLInputElement).value.trim(),
+              (passwordInput as HTMLInputElement).value.trim()
+            )
+          history.push("/")
+        } catch (error) {
+          setError(error.message)
         }
-      })
-      .then(() => {
-        history.push("/")
-      })
-      .catch((err) => {
-        throw new Error(err)
-      })
-  }
+      } else {
+        setError("You need to enter your email and password.")
+      }
+    },
+    [history]
+  )
   return (
-    <div>
+    <div className="my-4">
       <form onSubmit={submitForm} className="flex flex-col">
-        <input
-          type="text"
-          placeholder="Username"
-          className="p-4 mb-2 rounded border border-gray-300 focus:outline-none focus:shadow-outline"
-        />
         <input
           type="email"
           placeholder="Email"
@@ -48,6 +49,12 @@ const Login: React.FC<LoginProps> = ({ history }) => {
           placeholder="Password"
           className="p-4 mb-2 rounded border border-gray-300 focus:outline-none focus:shadow-outline"
         />
+        {error && (
+          <div className="flex items-center mb-2 p-2 rounded border border-red-700">
+            <ErrorLogo className="h-12 fill-current" />
+            <div className="ml-2 text-red-700">{error}</div>
+          </div>
+        )}
         <button
           type="submit"
           className="p-4 rounded bg-primary-700 hover:bg-primary-800 text-white focus:outline-none focus:shadow-outline"
