0c7e0bb115319a63f219b5d06f4fb7cf3d35800d
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-12-02 16:10:36 +0330

    Show last message time

--- src/components/home/chats/Message.tsx
+++ src/components/home/chats/Message.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react"
+import moment from "moment"
 import { UserContext } from "../../../contexts/UserContext"
 import { UserDataContext } from "../../../contexts/UserDataContext"
+import { Link } from "react-router-dom"
 
 interface MessageProps {
   user: string
@@ -13,9 +15,10 @@ const Message: React.FC<MessageProps> = ({ user }) => {
     message: string
     time: number
     from: string
-  }>()
+  } | null>(null)
 
   useEffect(() => {
+    // Sort from first to last and select the last message
     setLastMessage(
       userData.chats[user].sort((a, b) => a.time - b.time)[
         userData.chats[user].length - 1
@@ -25,24 +28,36 @@ const Message: React.FC<MessageProps> = ({ user }) => {
 
   return (
     <>
-      <div className="flex flex-col p-4">
+      <Link to={user} className="flex flex-col p-2 hover:bg-gray-100">
         {lastMessage && (
           <>
-            <div className="font-bold text-gray-900">{user}</div>
-            <div className="flex justify-between items-center text-gray-500">
+            <div className="flex justify-between items-center">
+              <div className="font-bold text-gray-900">
+                {user === currentUser?.displayName ? "Saved Messages" : user}
+              </div>
+              <div className="text-sm text-gray-500">
+                {new Date(lastMessage.time).getDay() === new Date().getDay()
+                  ? moment(lastMessage.time).format("h:mm a")
+                  : moment(lastMessage.time).format("Do MMM YY, h:mm a")}
+              </div>
+            </div>
+            <div className="flex items-center text-gray-500">
               <div className="flex items-center">
                 <div className="mr-1">
                   {lastMessage.from === currentUser?.displayName
                     ? "you:"
                     : lastMessage.from}
                 </div>
-                <div>{lastMessage.message.slice(0, 30)}...</div>
+                <div>
+                  {lastMessage.message.length >= 40
+                    ? `${lastMessage.message.slice(0, 40)}...`
+                    : lastMessage.message.slice(0, 40)}
+                </div>
               </div>
-              <div>20 min ago</div>
             </div>
           </>
         )}
-      </div>
+      </Link>
     </>
   )
 }
