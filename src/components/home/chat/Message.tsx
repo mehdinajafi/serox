07efdf813fb8408302934a8cbfecb1ace4a51951
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-12-04 18:39:26 +0330

    Fix bug in chat message

--- src/components/home/chat/Message.tsx
+++ src/components/home/chat/Message.tsx
@@ -15,15 +15,15 @@ const Message: React.FC<MessageProps> = ({ message }) => {
   return (
     <>
       {message.from === currentUser?.displayName ? (
-        <div className="flex flex-col my-2 p-2 w-max ml-auto bg-primary-800 rounded-t rounded-bl">
+        <div className="flex flex-col my-2 p-2 max-w-3/4 w-max ml-auto bg-primary-800 rounded-t rounded-bl">
           <div className="break-words text-white">{message.message}</div>
           <div className="ml-auto mt-1 text-sm text-gray-300">
             {moment(message.time).format("DD MMM, h:mm a")}
           </div>
         </div>
       ) : (
-        <div className="flex flex-col my-2 p-2 w-max mr-auto bg-gray-200 rounded-t rounded-br">
-          <div className="text-gray-900">{message.message}</div>
+        <div className="flex flex-col my-2 p-2 max-w-3/4 w-max mr-auto bg-gray-200 rounded-t rounded-br">
+          <div className="break-words text-gray-900">{message.message}</div>
           <div className="mr-auto mt-1 text-sm text-gray-600">
             {moment(message.time).format("DD MMM, h:mm a")}
           </div>
